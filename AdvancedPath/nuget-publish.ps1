3d1c36a19d0252286acb398607f094c5f650d634
Salih <salihozkara00@gmail.com>
2023-05-15 18:41:43 +0300

    Update version

--- AdvancedPath/nuget-publish.ps1
+++ AdvancedPath/nuget-publish.ps1
@@ -1,1 +1,3 @@
-$"dotnet nuget push .\bin\Release\*.nupkg --api-key $env:NUGET_API_KEY --source https://api.nuget.org/v3/index.json"
+dotnet build -c Release
+dotnet pack -c Release
+dotnet nuget push .\bin\Release\*.nupkg --api-key $env:NUGET_API_KEY --source https://api.nuget.org/v3/index.json --skip-duplicate
